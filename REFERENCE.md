78a627c34ca8458ea2ef456af637175e30785d4b
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-06-01 20:10:49 -0700

    readme reference

---
+++ REFERENCE.md
@@ -0,0 +1,19 @@
+# <Your-Project-Title>
+
+## Description
+
+## Table of Contents
+
+## Installation
+
+## Usage
+
+## Credits
+
+## License
+
+## Badges
+
+## How to Contribute
+
+## Tests
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkDown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of the project?", "What is the description?", "How do you install this?", "How do you use this?", "What are the contribution guidelines?", "What are the test instructions?"];
+
+// TODO: Write function for license list
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
