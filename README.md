7b6d4a6724f4467c726dc94aae33987b43bfdaba
chicken1991 <77861966+chicken1991@users.noreply.github.com>
2022-06-07 12:01:16 -0700

    everything works, TODO: cleanup license functions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dd
+# d
 
-   NOT LICENSED
+   [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
 
   ## Description
   d
@@ -44,8 +44,18 @@
   <a name="License"></a>
   ## License
 
-  ```md
-  COPYRIGHT 1991 undefined
-  undefined
-  ```
-  NOT LICENSED
+
+
+```md
+COPYRIGHT 2022 d
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+```      
+
+
+[License](https://opensource.org/licenses/BSD-3-Clause)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,13 +2,13 @@
 let badge;
 let lincenseLink;
 var licenseText;
-const year = Date.getFullYear(); //DO MOMENT HERE FOR CURRENT YEAR
+let year = new Date().getFullYear(); //DO MOMENT HERE FOR CURRENT YEAR
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
+function renderLicenseBadge(data) {
 
-  switch (license) {
+  switch (data.license) {
     case "MIT":
       badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
       break;
@@ -31,13 +31,13 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  switch (license) {
+function renderLicenseLink(data) {
+  switch (data.license) {
     case "MIT":
       lincenseLink = "[License: MIT](https://opensource.org/licenses/MIT)";
       licenseText = `
-      \`\`\`md
-      COPYRIGHT ${year} ${data.name}
+\`\`\`md
+COPYRIGHT ${year} ${data.name}
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -49,28 +49,28 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     case "Apache V.2":
       lincenseLink = "[License](https://opensource.org/licenses/Apache-2.0)"
       licenseText = `
-      \`\`\`md
-      COPYRIGHT ${year} ${data.name}
-      Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+\`\`\`md
+COPYRIGHT ${year} ${data.name}
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-  http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-   \`\`\`   
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+\`\`\`   
    `
       break;
     case "3-Clause BSD":
       lincenseLink = "[License](https://opensource.org/licenses/BSD-3-Clause)";
       licenseText = `
-      \`\`\`md
-      COPYRIGHT ${year} ${data.name}
-      Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+\`\`\`md
+COPYRIGHT ${year} ${data.name}
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
@@ -83,9 +83,9 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
     case "2-Clause BSD":
       lincenseLink = "[License](https://opensource.org/licenses/BSD-3-Clause)";
       licenseText = `
-      \`\`\`md
-      COPYRIGHT ${year} ${data.name}
-      Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+\`\`\`md
+COPYRIGHT ${year} ${data.name}
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
@@ -100,21 +100,21 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
     case "GNU General Public License version 3":
       lincenseLink = "[License: GPL v3](https://www.gnu.org/licenses/gpl-3.0)";
       licenseText = `
-      \`\`\`md
-      COPYRIGHT ${year} ${data.name}
-      This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+\`\`\`md
+COPYRIGHT ${year} ${data.name}
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    \`\`\`  
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+\`\`\`  
     `
       break;
     default:
@@ -129,8 +129,8 @@ function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  renderLicenseBadge(data.license);
-  renderLicenseLink(data.license);
+  renderLicenseBadge(data);
+  renderLicenseLink(data);
   return `# ${data.title}
 
    ${badge}
@@ -178,9 +178,9 @@ function generateMarkdown(data) {
   ## License
 
 
-  ${licenseText}
-  \`\`\`
-  ${lincenseLink}
+${licenseText}
+
+${lincenseLink}
 `;
 }
 
